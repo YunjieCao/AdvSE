8ea6498a9536249caa2831d4ec5225e17d3e79cd
Yunkai Tang <yut36@pitt.edu>
2020-03-01 22:37:05 -0500

    completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rottenpotatoes::Application.routes.draw do
   get 'static_pages/home'
 
   get 'static_pages/help'
+  get 'signup' => 'user_profiles#new'
 
   get 'movies/test'
   resources :movies
   # map '/' to be a redirect to '/movies'
   #root :to => redirect('/user_profiles')
-  root 'user_profiles#index'
+  root :to => redirect('static_pages/home')
   resources :users
   resources :user_profiles
   post 'users/register'
