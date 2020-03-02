68ef6f92361d57036af00c857e991cabd1087782
Yunkai Tang <yut36@pitt.edu>
2020-03-01 21:34:47 -0500

    Add a Static Pages controller
    '

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rottenpotatoes::Application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   get 'movies/test'
   resources :movies
   # map '/' to be a redirect to '/movies'
-  root :to => redirect('/user_profiles')
+  #root :to => redirect('/user_profiles')
+  root 'user_profiles#index'
   resources :users
   resources :user_profiles
   post 'users/register'
