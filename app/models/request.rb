73a1708caf6c3c620f4340996889e3555e6809fd
yunjie <YunjieCao@hotmail.com>
2020-03-05 16:27:19 -0500

    delete duplicate status helper code in Request.rb

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,23 +4,6 @@ class Request < ActiveRecord::Base
     return order.status
   end
 
-  def self.map_status(status)
-    case status
-    when 0
-      "Pending"
-    when 1
-      "Waiting carrier confirmation"
-    when 2
-      "Generated"
-    when 3
-      "Picked up by carrier"
-    when 4
-      "Arrive at desitination city"
-    when 5
-      "Finished"
-    end
-  end
-
   def self.update_status(order_id, status)
     order = Request.find_by(id:order_id)
     order.status = status
