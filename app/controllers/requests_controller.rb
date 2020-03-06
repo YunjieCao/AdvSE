77116d2f8c4113a3eb19965ba3a7351b3f290ea4
csw1995 <16904283+csw1995@users.noreply.github.com>
2020-03-06 01:25:23 +0000

    Show explanation of status code

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -19,6 +19,7 @@ class RequestsController < ApplicationController
     def create
       @request = Request.new(request_params)
       @request.requester_id = logged_id
+      @request.status = 0
       if @request.save
         flash[:success] = "New request successfully posted"
         redirect_to requests_path # should redirect to marketplace
