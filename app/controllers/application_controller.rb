4f306b588d0e2db331e26e28c6b136929d143b9d
yunjie <YunjieCao@hotmail.com>
2020-03-15 16:28:33 -0400

    define statuses as global var

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  $status_num = 7
+  $requester_unpaid = 0
+  $carrier_pending = 1
+  $carrier_unpaid = 2
+  $order_generated = 3
+  $order_picked_up = 4
+  $order_arrived = 5
+  $order_finished = 6
 end
