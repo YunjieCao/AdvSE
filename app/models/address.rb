73f96817afb7da568ce2b69a2b2675aa9e919b7a
csw1995 <16904283+csw1995@users.noreply.github.com>
2020-03-29 05:40:11 +0000

    Add link to new address in new request page

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,11 @@
 class Address < ActiveRecord::Base
+    validates :line1, presence: true, length: {maximum: 45}
+    validates :city, presence: true, length: {maximum: 45}
+    validates :state, presence: true, length: {maximum: 45}
+    validates :country, presence: true, length: {maximum: 45}
+    validates :postal_code, presence: true, length: {maximum: 45}
+    validates :phone_number, presence: true, length: {maximum: 45}
+
     def show
         return "#{self.line1}, #{self.line2}, #{self.city}, #{self.state}, #{self.country}, #{self.postal_code}, #{self.phone_number}"
     end
